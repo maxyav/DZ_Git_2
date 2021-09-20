bc839513df48eb0fc5a4d85f5f7f7c74e07106fc
maxyav <maxyav1982@gmail.com>
2021-09-20 22:35:11 +0300

    list_2.1

---
+++ DZ_Git_List_2.cpp
@@ -0,0 +1,20 @@
+// DZ_Git_List_2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+struct Node {
+    Node* next = nullptr;
+    int value;
+    Node(int value = 0) : value(value) {}
+};
+class List {
+public:
+    List() : _p_head(nullptr), _p_tail(nullptr) {}
+    ~List() {
+        while (_p_head != nullptr) {
+            Node* tmp = _p_head;
+            _p_head = _p_head->next;
+            delete tmp;
+        }
+    }
